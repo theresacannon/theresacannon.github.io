441ef09d1eb12b2721a69a2b99278fadac1cb2fe
Theresa Cannon <theresalynncannon@gmail.com>
2017-08-04 05:49:34 -0500

    Site created

---
+++ js/init.js
@@ -0,0 +1,14 @@
+(function($){
+  $(function(){
+
+    $(".button-collapse").sideNav();
+    $(".parallax").parallax();
+    $('.carousel').carousel();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+$(document).ready(function(){
+  $('.slider').slider();
+});
+    
