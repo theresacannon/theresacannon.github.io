3a015c6ada56c5d9a0b45fece2512a98f2bde78b
theresacannon <theresalynncannon@gmail.com>
2017-08-24 18:01:41 -0500

    update for 08_24_2017

--- updatePage.sh
+++ updatePage.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # get the current date to save it as the commit message
 now=$(date +"%m_%d_%Y")
+git pull --all
 git add --all
 git commit -m "update for $now"
 git push -u origin master
