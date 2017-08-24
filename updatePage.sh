ba167d7ba7f25444f9a450b8334f1c47bc083743
theresacannon <theresalynncannon@gmail.com>
2017-08-24 17:22:08 -0500

    Mom's first update for 08_24_2017

--- updatePage.sh
+++ updatePage.sh
@@ -2,5 +2,5 @@
 # get the current date to save it as the commit message
 now=$(date +"%m_%d_%Y")
 git add --all
-git commit -m "update for $now"
+git commit -m "Mom's first update for $now"
 git push -u origin master
